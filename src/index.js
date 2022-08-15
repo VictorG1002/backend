20902feb5010ed20cd39066e1b9c23afcef71829
Victor <vggaia10@gmail.com>
2022-08-15 12:33:33 -0300

    create post method

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: 'Hello World'
-  })
+app.get('/projects', (request, response) => {
+  return response.json(['Projeto 1', 'Projeto 2'])
+})
+
+app.post('/projects', (request, response) => {
+  return response.json(['Projeto 1', 'Projeto 2'])
 })
 
 app.listen(3333, () => {
