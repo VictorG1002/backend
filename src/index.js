6884f5e686167d6a11d35360684b306e72c84d71
Victor <vggaia10@gmail.com>
2022-08-15 12:47:33 -0300

    add remove method

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ app.post('/projects', (request, response) => {
   return response.json(['Projeto 1', 'Projeto 2'])
 })
 
+app.put('/project/:id', (request, response) => {
+  return response.json(['Projeto 3', 'Projeto 2'])
+})
+
+app.delete('/project/:id', (request, response) => {
+  return response.json(['Projeto 3', 'Projeto 2'])
+})
+
 app.listen(3333, () => {
   console.log('Backend started 🥶')
 })
