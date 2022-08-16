d73f37994f78a68db4fb2d437fae8925e300fdf7
Victor <vggaia10@gmail.com>
2022-08-16 09:02:44 -0300

    study query params

--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ const express = require('express')
 const app = express()
 
 app.get('/projects', (request, response) => {
+  const { owner, project } = request.query
+  console.log(owner)
+  console.log(project)
+
   return response.json(['Projeto 1', 'Projeto 2'])
 })
 
 app.post('/projects', (request, response) => {
-  return response.json(['Projeto 1', 'Projeto 2'])
+  return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3'])
 })
 
-app.put('/project/:id', (request, response) => {
+app.put('/projects/:id', (request, response) => {
   return response.json(['Projeto 3', 'Projeto 2'])
 })
 
-app.delete('/project/:id', (request, response) => {
-  return response.json(['Projeto 3', 'Projeto 2'])
+app.delete('/projects/:id', (request, response) => {
+  return response.json(['Projeto 2'])
 })
 
 app.listen(3333, () => {
